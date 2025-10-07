22ecde2469ef30b4a78c7d3b30a7f023cf355580
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-10-07 17:33:11 +0530

    refactor: update JansNewPasswordService.java

--- lib/org/gluu/agama/login/jans/JansNewPasswordService.java
+++ lib/org/gluu/agama/login/jans/JansNewPasswordService.java
@@ -229,8 +229,9 @@ public class JansNewPasswordService extends NewPasswordService {
 
             // Localized message
             Map<String, String> messages = new HashMap<>();
-            
-            messages.put("ar", "\u200Fرمز Phi Wallet الخاص بك هو \u200F" + otpCode + ". لا تشاركه مع أي شخص.");
+
+            messages.put("ar", "رمز Phi Wallet الخاص بك هو " + otpCode + ". لا تشاركه مع أي شخص.
+");
             messages.put("en", "Your Phi Wallet OTP is " + otpCode + ". Do not share it with anyone.");
             messages.put("es", "Tu código de Phi Wallet es " + otpCode + ". No lo compartas con nadie.");
             messages.put("fr", "Votre code Phi Wallet est " + otpCode + ". Ne le partagez avec personne.");
